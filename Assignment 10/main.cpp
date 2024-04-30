5827fa461103ec1daf52cf54c2b6fc247e2cbaff
ethanenkhtur <98515458+ethanenkhtur@users.noreply.github.com>
2024-04-30 12:09:45 -0500

    Assignment 10 done. Just missing documentation. Also waiting on revision from professor

--- Assignment 10/main.cpp
+++ Assignment 10/main.cpp
@@ -40,19 +40,54 @@ class Employee {
 };
 
 int main() {
-    Employee first_employee = Employee("Ethan", "NIU4274", 53498.61);
+    Employee first_employee = Employee("Ethan Enkhtur", "NIU4274", 53498.61);
 
     cout << "The first Employee object:" << endl;
     first_employee.display();
     first_employee.increaseSalary(125.15);
     first_employee.display();
 
+
+    Employee second_employee = Employee();
+
+    cout << endl << endl << "The second Employee object:" << endl;
+    second_employee.display();
+    second_employee.increaseSalary(-2200.00);
+    second_employee.display();
+
+
+    Employee third_employee = Employee("Blanche Devereaux", "TGG1985", 820.12);
+
+    cout << endl << endl << "The third Employee object:" << endl;
+    third_employee.display();
+    third_employee.setSalary(82.88);
+    third_employee.setIDnum("TGP1992");
+    third_employee.display();
+
+
+    Employee fourth_employee = Employee("Grace Hopper", "UNI1950", 2468.00);
+
+    cout << endl << endl << "The fourth Employee object:" << endl;
+    fourth_employee.display();
+    fourth_employee.setIDnum("COBOL1959");
+    fourth_employee.increaseSalary(9517.53);
+    cout << fourth_employee.getName() << " has a salary of " << fourth_employee.getSalary() << endl << endl;
+    fourth_employee.display();
+
+
+    Employee fifth_employee = Employee("Charlie Hudson", "REX2332", 71940.76);
+
+    cout << endl << endl << "The fifth Employee object:" << endl;
+    fifth_employee.display();
+    fifth_employee.setSalary(-10000.01);
+    cout << endl << "Employee 5 has an identification number of " << fifth_employee.getIDnum() << endl << endl;
+    fifth_employee.display();
 }
 
 Employee::Employee() {
     strcpy(name, "None");
     setIDnum("ACB1234");
-    setSalary(0);
+    salary = 0;
 }
 
 Employee::Employee(const char newName[], const char id[], double newSalary) {
@@ -72,7 +107,7 @@ void Employee::increaseSalary(double raise) {
     if (raise > 0) 
         salary += raise;
     else
-        cout << "Error: salary increase is invalid - negative or 0.00. Salary was not changed." << endl;
+        cout << "Error: salary increase is invalid - negative or 0.00. Salary was not changed." << endl << endl;
 }
 
 void Employee::setIDnum(const char updated_id[]) {
@@ -91,11 +126,11 @@ void Employee::setIDnum(const char updated_id[]) {
 
 
     if (strlen(updated_id) != 7) {
-        cout << "Error: new id is invalid - not 7 characters. ID was not changed." << endl;
+        cout << "Error: new id is invalid - not 7 characters. ID was not changed." << endl << endl;
     } else if (!alphabetics) {
-        cout << "Error: new id is invalid - not first 3 alphabetics. ID was not changed." << endl;
+        cout << "Error: new id is invalid - not first 3 alphabetics. ID was not changed." << endl << endl;
     } else if (!digits) {
-        cout << "Error: new id is invalid - not last 4 digits. ID was not changed." << endl;
+        cout << "Error: new id is invalid - not last 4 digits. ID was not changed." << endl << endl;
     } else {
         strcpy(id, updated_id);
     }
